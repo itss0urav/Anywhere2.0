9f1cc2b03fb6702b154dceec734321cb65873599
itss0urav <sourav2021official@gmail.com>
2023-12-09 13:05:56 +0530

    updates

--- anywhere2.0/client/src/components/Navbar.jsx
+++ anywhere2.0/client/src/components/Navbar.jsx
@@ -1,24 +1,28 @@
 import React from "react";
+import Logo from "../assets/Anywhere-Transparent.png";
+import { MdContactMail, MdBuild, MdInfo } from "react-icons/md";
 
 const Navbar = () => {
   return (
     <nav className="bg-gradient-to-r from-blue-700 to-blue-500 p-4 backdrop-blur-lg bg-opacity-40 border border-blue-300 border-opacity-20">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white font-bold text-xl">Logo</div>
-        <div className="space-x-4">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+        <div className="text-white font-bold text-xl flex items-center mb-4 md:mb-0">
+          <img className="w-12 mr-2" src={Logo} alt="" />
+        </div>
+        <div className="flex items-center space-x-4">
           <input
-            className="rounded p-2 border  transition-all duration-200"
+            className="rounded p-2 border transition-all duration-200"
             type="text"
             placeholder="Search..."
           />
           <button className="bg-blue-800 text-blue-100 rounded px-4 py-2 transition-all duration-200 hover:bg-blue-500 hover:text-white">
-            About
+            <MdInfo className="inline-block mr-1"/> About
           </button>
           <button className="bg-blue-800 text-blue-100 rounded px-4 py-2 transition-all duration-200 hover:bg-blue-500 hover:text-white">
-            Services
+            <MdBuild className="inline-block mr-1"/> Services
           </button>
           <button className="bg-blue-800 text-blue-100 rounded px-4 py-2 transition-all duration-200 hover:bg-blue-500 hover:text-white">
-            Contact Us
+            <MdContactMail className="inline-block mr-1"/> Contact Us
           </button>
         </div>
       </div>
