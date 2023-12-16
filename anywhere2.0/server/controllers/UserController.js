1b0dc54c567aa752ab5c5fec918fd0acd13adfd4
itss0urav <sourav2021official@gmail.com>
2023-12-16 14:50:33 +0530

    cleaning code

--- anywhere2.0/server/controllers/UserController.js
+++ anywhere2.0/server/controllers/UserController.js
@@ -5,23 +5,15 @@ const UserController = {
   createUser: async (req, res) => {
     try {
       const { username, dob, email, password } = req.body;
-
-      // Perform any necessary validation here
-
-      // Hash the password before saving it to the database
       const salt = await bcrypt.genSalt(10);
       const hashedPassword = await bcrypt.hash(password, salt);
-
-      // Create a new user in the database
       const newUser = new User({
         username,
         dob,
         email,
         password: hashedPassword,
       });
-
       await newUser.save();
-
       res.status(201).json({ message: "User created successfully" });
     } catch (error) {
       console.error(error);
@@ -32,23 +24,16 @@ const UserController = {
   loginUser: async (req, res) => {
     try {
       const { username, password } = req.body;
-
-      // Find the user by username
       const user = await User.findOne({ username });
-
       if (!user) {
         return res.status(404).json({ message: "User not found" });
       }
-
-      // Compare the entered password with the hashed password in the database
       const passwordMatch = await bcrypt.compare(password, user.password);
-
       if (!passwordMatch) {
         return res.status(401).json({ message: "Incorrect password" });
       }
-
+      
       // Implement token generation or session management for successful login
-
       res.status(200).json({ message: "Login successful" });
     } catch (error) {
       console.error(error);
--- anywhere2.0/server/models/User.js
+++ anywhere2.0/server/models/User.js
@@ -1,10 +1,22 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  dob: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
+  username: {
+    type: String,
+    required: true,
+  },
+  dob: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
 const User = mongoose.model("User", userSchema);
