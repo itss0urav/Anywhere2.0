47f717116d720d90b0b591c0bea35e364119d6ab
itss0urav <sourav2021official@gmail.com>
2024-01-08 18:24:48 +0530

    profile page update

--- anywhere2.0/client/src/pages/UserProfile.jsx
+++ anywhere2.0/client/src/pages/UserProfile.jsx
@@ -6,25 +6,40 @@ export default function UserProfile() {
   const { user } = useContext(GlobalContext);
   useEffect(() => {}, [user]);
 
-//   console.log(user.userCredentials.username);
   return (
     <div className="">
-      <Navbar />
-      <div className="bg-white shadow overflow-hidden sm:rounded-lg">
-        <div className="px-4 py-5 sm:px-6">
-          <h3 className="text-lg leading-6 font-medium text-gray-900">
-            User Profile
-          </h3>
-        </div>
-        <div className="border-t border-gray-200">
-          <dl>
-            <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt className="text-sm font-medium text-gray-500">Username</dt>
-              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                {user.user.username}
-              </dd>
-            </div>
-          </dl>
+        <Navbar />
+      <div className="container mx-auto px-4">
+        <div className="bg-white shadow-md rounded-lg p-6 mt-4">
+          <div className="mb-4">
+            <h3 className="text-xl text-center font-semibold text-gray-900">
+              User Profile
+            </h3>
+          </div>
+          <div className="border-t border-gray-200">
+            <dl>
+              <div className="grid grid-cols-3 gap-4 py-4">
+                <dt className="text-sm font-medium text-gray-500">Username</dt>
+                <dd className="mt-1 text-sm text-gray-900 col-span-2">
+                  {user.user.username}
+                </dd>
+              </div>
+              <div className="grid grid-cols-3 gap-4 py-4">
+                <dt className="text-sm font-medium text-gray-500">
+                  Date Of Birth
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900 col-span-2">
+                  {user.user.dob}
+                </dd>
+              </div>
+              <div className="grid grid-cols-3 gap-4 py-4">
+                <dt className="text-sm font-medium text-gray-500">Email</dt>
+                <dd className="mt-1 text-sm text-gray-900 col-span-2">
+                  {user.user.email}
+                </dd>
+              </div>
+            </dl>
+          </div>
         </div>
       </div>
     </div>
