751254d58604d4d6a456872bf011dbb7b91e9a6d
itss0urav <sourav2021official@gmail.com>
2024-01-13 13:53:15 +0530

    ui updates for comment

--- anywhere2.0/client/src/components/CommentContainer.jsx
+++ anywhere2.0/client/src/components/CommentContainer.jsx
@@ -5,7 +5,6 @@ import axios from "../config/axios";
 
 export default function CommentContainer() {
   const { postId } = useParams();
-  console.log("From container", postId);
   const [comments, setComments] = useState([]);
   const [votes, setVotes] = useState(0);
 
@@ -21,6 +20,7 @@ export default function CommentContainer() {
 
     fetchComments();
   }, [postId]);
+
   const upvote = () => {
     setVotes(votes + 1);
   };
@@ -30,30 +30,23 @@ export default function CommentContainer() {
   };
 
   return (
-    <div className="p-4 max-w-xl mx-auto bg-white rounded-xl shadow-md space-y-2 sm:py-4">
-      <div>CommentContainer</div>
-      <div>
-        {comments.map((comment) => (
-          <div key={comment._id}>
-            <h2 className="flex gap-1">
-              Comment By{" "}
-              <div className="bg-gradient-to-r from-sky-400 to-indigo-600 bg-clip-text text-transparent">
-                {comment.user}
-              </div>{" "}
-            </h2>
-            <p className="bg-gradient-to-r from-cyan-900 to-sky-700 bg-clip-text text-transparent">{comment.text}</p>
-            <div className="flex items-center space-x-2">
-              <button onClick={upvote} className="text-green-500">
-                <LuArrowBigUp />
-              </button>
-              <button onClick={downvote} className="text-red-500">
-                <LuArrowBigDown />
-              </button>
-              <span>{votes}</span>
-            </div>
+    <div className="p-4 max-w-xl mx-auto bg-white rounded-xl shadow-md space-y-4 sm:py-4">
+      <h2 className="text-3xl font-bold text-center">Comments</h2>
+      {comments.map((comment) => (
+        <div className="bg-gray-100 p-4 rounded-md" key={comment._id}>
+          <h3 className="text-xl font-bold">{comment.user}</h3>
+          <p className="text-gray-700">{comment.text}</p>
+          <div className="flex items-center space-x-2 mt-2">
+            <button onClick={upvote} className="text-green-500">
+              <LuArrowBigUp />
+            </button>
+            <button onClick={downvote} className="text-red-500">
+              <LuArrowBigDown />
+            </button>
+            <span>{votes}</span>
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
   );
 }
